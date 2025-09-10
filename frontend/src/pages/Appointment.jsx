f72e5796e056b3f1e933dd4e74f6a141b6cbe1a9
Thyagarajan-B <thyagarajanb53@gmail.com>
2025-09-10 20:48:22 +0530

    nothing

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -102,7 +102,7 @@ const Appointment = () => {
         try {
             const { data } = await axios.post(
                 backendUrl + '/api/user/book-appointment',
-                { userId: localStorage.getItem("userId"), docId, slotDate, slotTime },
+                { docId, slotDate, slotTime },
                 { headers: { token } }
             );
 
