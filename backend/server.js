e5c5a21a967dba3c0f5fc3fae9079e8bc71bc30d
sanjay N <sanjaynagarju03@gmail.com>
2025-09-26 19:18:18 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,15 @@ import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
 import bodyParser from "body-parser";
-import expressSession from "express-session"
-import passport from "passport"
+// import expressSession from "express-session"
+// import passport from "passport"
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
 import "./config/passport.js"
-import MongoStore from "connect-mongo"
+// import MongoStore from "connect-mongo"
 
 // app config
 const app = express()
@@ -19,21 +19,21 @@ connectDB()
 connectCloudinary()
 
 
-app.use(
-  expressSession({
-    secret: process.env.EXPRESS_SECRET, // required!
-    resave: false,       // don't save session if unmodified
-    saveUninitialized: false, // only save if something stored
-    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // optional
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
-  })
-);
+// app.use(
+//   expressSession({
+//     secret: process.env.EXPRESS_SECRET, // required!
+//     resave: false,       // don't save session if unmodified
+//     saveUninitialized: false, // only save if something stored
+//     store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // optional
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24, // 1 day
+//     },
+//   })
+// );
 
 app.use(cors());
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // api endpoints
